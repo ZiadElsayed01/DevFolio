25258149dc0084387e7644df22e3162eac386c5e
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2024-05-02 16:40:57 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+var navBar = document.getElementsByClassName("navbar")[0];
+var arrow = document.getElementsByClassName("arrow")[0];
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+  var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > 50) {
+    navBar.style.background = "#000000";
+    arrow.style.display = "block";
+  } else {
+    navBar.style.background = "";
+    arrow.style.display = "none";
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
